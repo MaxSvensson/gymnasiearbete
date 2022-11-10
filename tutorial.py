66c7ea1223d3465184b4ea0a3bc584287e77d30e
SickanK <sixtenklementsson@protonmail.com>
2022-11-10 17:06:23 +0100

    working ai

--- tutorial.py
+++ tutorial.py
@@ -1,3 +1,5 @@
+# conda activate base
+
 import numpy as np
 import torch 
 import torch.nn as nn
@@ -9,26 +11,30 @@ from helpers import hoursSinceEpoch
 device = "mps" if torch.backends.mps.is_available() else "cpu"
 print(f"Using device: {device}")
 
-df = pd.read_csv("data_day.csv", sep=';')
+#
+# CONFIG DATASET
+#
 
-# Remove uncompleted set
-df.drop('Torkel Knutssonsg', axis=1, inplace=True)
-
-# Remove last undefined set
-df.drop("Null", axis=1, inplace=True)
+df = pd.read_csv("lunden.csv", sep=';')
 
 # Drop missing records 
-df.dropna(subset=["Marsta", "Norr Malma", "Högdalen"], inplace=True)
+df.dropna(subset=["Dygnsmedel"], inplace=True)
 df.reset_index(drop=True, inplace=True)
 
-dataset = df.iloc[:,2].values
-train_dataset = dataset[0:600]
-test_dataset = dataset[hoursSinceEpoch(600):hoursSinceEpoch(630)]
+dataset = df.iloc[:,1].values
+dataset = dataset.astype(float)
+
+train_window_end = 718
+train_dataset = dataset[0:train_window_end]
 
 scaler = MinMaxScaler(feature_range=(-1, 1))
 
 train_data_normalized = scaler.fit_transform(train_dataset.reshape(-1, 1))
 train_data_normalized = torch.FloatTensor(train_data_normalized).view(-1).to(device)
+
+#
+# TRAIN
+#
 
 train_window = 30;
 
@@ -60,24 +66,58 @@ class LSTM(nn.Module):
         predictions = self.linear(lstm_out.view(len(input_seq), -1))
         return predictions[-1]
 
+#
+# TEST
+#
+
+from_save = True
+model_path = "./model_3"
 model = LSTM().to(device)
-loss_function = nn.MSELoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
-epochs = 150 
+if (from_save == False):
+    loss_function = nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
-for i in range(epochs):
-    for seq, labels in train_inout_seq:
-        optimizer.zero_grad()
-        model.hidden_cell = (torch.zeros(1, 1, model.hidden_layer_size).to(device),
+    epochs = 50
+
+    for i in range(epochs):
+        for seq, labels in train_inout_seq:
+            optimizer.zero_grad()
+            model.hidden_cell = (torch.zeros(1, 1, model.hidden_layer_size).to(device),
+                            torch.zeros(1, 1, model.hidden_layer_size).to(device))
+
+            y_pred = model(seq)
+
+            single_loss = loss_function(y_pred, labels)
+            single_loss.backward()
+            optimizer.step()
+
+        print(f'epoch: {i:3} loss: {single_loss.item():10.8f}')
+
+    print(f'epoch: {i:3} loss: {single_loss.item():10.10f}')
+
+    torch.save(model.state_dict(), model_path)
+    print("Model saved to: ", model_path)
+else:
+    model.load_state_dict(torch.load(model_path))
+
+
+fut_pred = 12
+
+test_inputs = train_data_normalized[-train_window:].tolist()
+
+model.eval()
+
+
+for i in range(fut_pred):
+    seq = torch.FloatTensor(test_inputs[-train_window:]).to(device)
+    with torch.no_grad():
+        model.hidden = (torch.zeros(1, 1, model.hidden_layer_size).to(device),
                         torch.zeros(1, 1, model.hidden_layer_size).to(device))
+        test_inputs.append(model(seq).item())
 
-        y_pred = model(seq)
+test_inputs[fut_pred:]
+actual_predictions = scaler.inverse_transform(np.array(test_inputs[train_window:] ).reshape(-1, 1))
 
-        single_loss = loss_function(y_pred, labels)
-        single_loss.backward()
-        optimizer.step()
-
-    print(f'epoch: {i:3} loss: {single_loss.item():10.8f}')
-
-print(f'epoch: {i:3} loss: {single_loss.item():10.10f}')
+print("Predictions: ", actual_predictions)
+print("Real values: ", dataset[train_window_end:train_window_end + fut_pred])
